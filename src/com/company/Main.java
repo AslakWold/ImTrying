b032b9659e67746784e08d17a1190ad1e8b54c7e
AslakWold <s331378@oslomet.no>
2019-12-03 17:12:33 +0100

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         int[] b = {0,0,1,0,0,1,0,1,1,0,0,1,1,1,0,1,0,1,1,0};
         int[] a = {6, 10, 9, 4, 1, 3, 8, 5};
-
+        //Kuk
         Oblig1.delsortering(a);
         System.out.println(Arrays.toString(a));
 
